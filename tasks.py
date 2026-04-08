84993be041134101101c0c2b96422c8b1f80c34f
Scott Arne Johnson <scott.johnson@bms.com>
2026-04-08 16:31:06 -0700

    Update project dependencies and packaging for vrzn CLI tool

--- tasks.py
+++ tasks.py
@@ -1,5 +1,4 @@
 import sys
-# noinspection PyPackageRequirements
 from invoke.tasks import task
 from pathlib import Path
 
@@ -8,19 +7,11 @@ ROOT = Path(__file__).parent.absolute()
 
 @task
 def test(c):
-    c.run(f'cd {ROOT} && {sys.executable} -m unittest')
+    c.run(f"cd {ROOT} && {sys.executable} -m pytest")
+
 
 @task
 def build(c):
-    c.run(f'rm -rf {ROOT / "dist"}')
-    c.run(f'cd {ROOT} && {sys.executable} -m build')
-
-@task
-def upload(c):
-    c.run(f'scp {ROOT}/dist/* hopi36.pri.bms.com:/web/msdpypi/packages/ && echo "Successfully uploaded..."')
-
-@task
-def deploy(c):
-    build(c)
-    upload(c)
+    c.run(f"rm -rf {ROOT / 'dist'}")
+    c.run(f"cd {ROOT} && {sys.executable} -m build")
 
